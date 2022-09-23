58ca2465af1e91edf39b093913e0211a931df47d
505 <505@academicos.cesde.edu.co>
2022-09-23 17:44:45 -0500

    continuidad clase9

---
+++ analisisempleado.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+tablaEmpleados=pd.read_csv('./empleados.csv')
+#print(tablaEmpleados.to_string())
+
+
+# filtro 1 quiero obtener todos los datos de los analistas 1
+#tablaAnalista1=tablaEmpleados[tablaEmpleados["cargo"]=="analista1"].head(50)
+
+#archivoHTML=tablaAnalista1.to_html()
+#archivoTEXTO=open("datosanalistas1.html","w")
+#archivoTEXTO.write(archivoHTML)
+#archivoTEXTO.close()
+
+tablaAnalista2=tablaEmpleados[tablaEmpleados["cargo"]=="analista2"].head(50)
+archivoHTML=tablaAnalista2.to_html()
+archivoTEXTO=open("datosanalistas2.html","w")
+archivoTEXTO.write(archivoHTML)
+archivoTEXTO.close()
